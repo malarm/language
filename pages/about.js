f6be4f777e683ecb82656822c9f3631bc9a73518
Malar Ruban <mru@wineandbarrels.com>
2021-01-12 19:54:00 +0100

    updated about page

--- pages/about.js
+++ pages/about.js
@@ -6,9 +6,7 @@ export default () => (
   <div className="mt-10 p-10 shadow rounded bg-white min-h-70v ">
     <PageHeader title="About Us"/>
     <div className="text-gray-700 space-y-5"> 
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fringilla tempus rhoncus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque arcu metus, imperdiet vel justo sit amet, maximus molestie neque. Vivamus faucibus non sapien a dapibus. Morbi auctor laoreet leo vel rutrum. Nullam tincidunt dui iaculis malesuada vehicula. Integer tincidunt nec arcu eu vehicula. Fusce libero tortor, pharetra a bibendum sit amet, accumsan ac sapien. Duis aliquet magna sem, sed volutpat est facilisis vel.</p>
-      
-      <p>Nam mollis ultrices ultrices. Nam interdum quis lacus et sodales. Sed egestas id eros non lobortis. Etiam ultricies sem eros, non imperdiet diam pretium sed. Curabitur in enim metus. Quisque tincidunt aliquam odio. In sed eros orci. Morbi vel condimentum metus. Sed nec eleifend libero. Ut eu lectus eget nunc tempor commodo fringilla molestie enim. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi et ex sed dolor condimentum pharetra. Aenean ut ullamcorper ante, quis aliquet nulla. Ut iaculis ligula eget urna convallis aliquam. Vestibulum justo sapien, auctor ut laoreet non, fermentum mollis nisi. Sed tristique nunc eget pharetra accumsan.</p>
+     
       
       <p>Morbi hendrerit nibh vitae ex sodales molestie. Curabitur nec ullamcorper quam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec vel urna quis tortor sagittis tincidunt et non augue. Maecenas lectus mi, feugiat eu ullamcorper sed, vulputate et magna. Aenean tempus mattis posuere. Phasellus finibus cursus vehicula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aliquam eget sem sit amet risus ultricies gravida quis sed nibh. Integer aliquam molestie elementum.</p>
       
